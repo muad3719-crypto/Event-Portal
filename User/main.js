31d0113a51508d4980c8ff782ac3fa9c68be4b99
ayoelhd <215073788+ayoelhd@users.noreply.github.com>
2025-11-08 23:04:58 +0200

    added nav/footer+fixed link between login and user

--- User/main.js
+++ User/main.js
@@ -31,73 +31,7 @@ if (!users.find(u => u.email === "admin@limu.edu.ly")) {
 // =====================
 // SIGNUP
 // =====================
-document.addEventListener("DOMContentLoaded", () => {
-  const signupForm = document.getElementById("signupForm");
-  if (signupForm) {
-    signupForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const name = document.getElementById("name").value.trim();
-      const email = document.getElementById("email").value.trim();
-      const password = document.getElementById("password").value.trim();
 
-      if (!name || !email || !password) {
-        alert("⚠️ Please fill all fields!");
-        return;
-      }
-
-      const users = JSON.parse(localStorage.getItem("users"));
-      if (users.find(u => u.email === email)) {
-        alert("❌ Email already exists!");
-        return;
-      }
-
-      users.push({ name, email, password, role: "user" });
-      localStorage.setItem("users", JSON.stringify(users));
-      alert(`🎉 Welcome ${name}! Your account has been created. Redirecting to login...`);
-      setTimeout(() => {
-        window.location.href = "../Ulogin.html";
-      }, 500);
-    });
-  }
-
-  // =====================
-  // LOGIN
-  // =====================
-  const loginForm = document.getElementById("loginForm");
-  if (loginForm) {
-    loginForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const email = document.getElementById("email").value.trim();
-      const password = document.getElementById("password").value.trim();
-
-      if (!email || !password) {
-        alert("⚠️ Please fill all fields!");
-        return;
-      }
-
-      const users = JSON.parse(localStorage.getItem("users"));
-      const user = users.find(u => u.email === email && u.password === password);
-
-      if (!user) {
-        alert("❌ Invalid email or password!");
-        return;
-      }
-
-      localStorage.setItem("currentUser", JSON.stringify(user));
-
-      alert(`✅ Login successful! Welcome ${user.name}`);
-
-      if (user.role === "admin") {
-        setTimeout(() => {
-          window.location.href = "admin/dashboard.html";
-        }, 300);
-      } else {
-        setTimeout(() => {
-          window.location.href = "USER/dashboard.html";
-        }, 300);
-      }
-    });
-  }
 
   // =====================
   // LOGOUT
@@ -127,4 +61,4 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "../Ulogin.html";
     }
   }
-});
+
